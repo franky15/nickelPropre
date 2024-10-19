871f7dddb19c3d09e0cafa36dbd377c676191399
fr <fr@MacBook-Air-de-fr.local>
2024-10-19 04:06:51 +0200

    déindexation de .env

--- back/app.js
+++ back/app.js
@@ -5,16 +5,36 @@ const cors = require("cors");
 //importation du module path pour la gestion des chemins de fichiers à cause de multer qui va enregistrer les fichiers (images)
 const path = require('path');
 
+
+
+/**
+ * permet d'interagir avec le système de fichiers. Il fournit des méthodes pour lire, écrire, supprimer et manipuler des fichiers et des répertoires.
+ */
 const fs = require('fs');
 
 //importation du package pour les variables d'environnement
 const dotenv = require("dotenv").config();
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); //permet d'avoir accès aux données de la requête dans req.body 
+
 
 
 
 //importation de la base de données
-const db = require("./mysql.config")        
+// const db = require("./mysql.config")   
+
+let db 
+//pour savoir si on est en mode production ou développement ce qui permettra de configurer l'application en fonction de l'environnement local ou de production par exemple base de données dlocale ou distante (en ligne RDS d'AWS ou autre)
+if (process.env.NODE_ENV === 'production') {
+
+  console.log('*****L’application est en mode production.*****');
+  // Configurations spécifiques à la production
+
+} else {
+
+  console.log('*****L’application est en mode développement(en local).*****');
+  
+   db = require("./mysql.config")  
+}
  
 //importation des modules de routages 
 
@@ -30,8 +50,8 @@ const stripeController = require('./controllers/stripeController'); // Ajoute di
 //creation de l'api
 const app = express();      
 
-
-var __dirname = path.resolve();  //récupération du chemin absolu du répertoire du fichier actuel si non on aura une erreur sur __dirname
+//exemple Supposons que le répertoire courant soit /home/user/project. Dans ce cas, path.resolve('images', 'photo.jpg') renverra /home/user/project/images/photo.jpg.
+var __dirname = path.resolve();  //Résout une séquence de chemins en un chemin absolu, en utilisant le répertoire courant comme base du chemin absolu. puis le reste du chemin est ajouté à ce chemin absolu
 app.use(cors());   //evite les erreurs cors 
 
 
@@ -48,6 +68,7 @@ app.post('/stripe/stripe-webhook', bodyParser.raw({ type: 'application/json' }),
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
+//app.use() permet d'utiliser un middleware qui est une fonction qui reçoit les objets request et response et qui peut effectuer une action sur ces objets et est contacté par le chemin de la requête
 app.use("/users", userRoutes); 
 app.use("/chantiers", chantierRoutes); 
 app.use("/articles", articleRoutes);
@@ -59,8 +80,10 @@ app.use("/stripe", stripeRoutes);
 
 
 
-
-app.use('/images', express.static(path.join(__dirname, 'images')));  
+//configure un chemin statique pour servir des fichiers d'image.
+//c'est ce middleware qui permet de servir ou de gérer les "images"  qui sont dans le dossier /images
+//__dirname est le chemin absolu du répertoire du fichier actuel
+app.use( '/images', express.static(path.join(__dirname, 'images')) );  
 
 
 module.exports = app;  
